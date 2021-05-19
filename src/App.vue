92e76db20714212fcf1ca178d6c5c481b871aa1f
fakefate14 <farm_rs106@hotmail.com>
2021-05-19 23:59:16 +0700

    learning vuex

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |||
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<div id="app">
+  <comA/>
+  <hr>
+  <h1>{{$store.state}}</h1>
+</div>
 </template>
 
+<script>
+import ComA from "@/components/ComA.vue"
+export default {
+  name:"App",
+  components:{
+    ComA
+  },
+  mounted(){
+    
+    setInterval(()=>{this.$store.state.count ++ },100)
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/ComA.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="comA">
+      <h1>{{$store.state.count}}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name:"ComA",
+  mounted(){
+    
+    setInterval(()=>{this.$store.state.count -- },100)
+  }
+}
+</script>
+
+<style>
+
+</style>
